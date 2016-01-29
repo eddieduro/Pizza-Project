6a42d8c74c6441b8f71308440abee105e239d550
eddie <e.jamesduro@gmail.com>
2016-01-29 10:02:59 -0800

    order form functionality

--- js/scripts.js
+++ js/scripts.js
@@ -15,5 +15,30 @@ Pizza.prototype.addBacon = function(){
 $(document).ready(function(){
 	$('form#orderPizza').submit(function(event){
 		event.preventDefault();
+		var sizeValue = parseInt($('#sizeSelect option:selected').val());
+		var toppingValue = parseInt($('#toppingSelect option:selected').val());
+		
+		var size = $('#sizeSelect option:selected').text();
+		var topping = $('#toppingSelect option:selected').text();
+		var newPizza = new Pizza(size, topping);
+		var totalCost = sizeValue + toppingValue;
+		var pizzaType;
+
+		if(topping === "Pepperoni"){
+			newPizza.addPepperoni();
+		} else if (topping === "Bacon") {
+			newPizza.addBacon();
+		}
+ 	$('#orderPizza').slideUp(280).dequeue().fadeOut(150);
+ 	$('#results').slideDown(250).dequeue().fadeIn(200);
+		if(newPizza.pepperoni){
+			pizzaType = "Pepperoni Pizza";
+			$('#results').prepend("<p id='receipt'>You ordered a " + size + " " + pizzaType +".</p><br/>");
+			$('#receipt').append('<p>Your total comes out to $' + totalCost + " flat - got to love no tax in Oregon!</p>");
+		} else if (newPizza.bacon){
+			pizzaType = "Bacon Pizza"
+			$('#results').prepend("<p id='receipt'>You ordered a " + size + " " + pizzaType +".</p><br/>");
+			$('#receipt').append('<p>Your total comes out to $' + totalCost + " flat - got to love no tax in Oregon!</p>");
+		}
 	});
 });
